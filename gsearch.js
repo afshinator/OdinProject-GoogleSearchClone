4070884d30737beec392f0d72d1fdddd90c9df5c
Afshin Mokhtari <afshinator@hotmail.com>
2014-02-23 02:10:41 -0800

    First commit

---
+++ gsearch.js
@@ -0,0 +1,10 @@
+	(function() {
+		var cx = '012848941596393641026:1ciam8frko0';		// TODO: Put in your API key here.
+		var gcse = document.createElement('script');
+		gcse.type = 'text/javascript';
+		gcse.async = true;
+		gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+		'//www.google.com/cse/cse.js?cx=' + cx;
+		var s = document.getElementsByTagName('script')[0];
+		s.parentNode.insertBefore(gcse, s);
+	})();
